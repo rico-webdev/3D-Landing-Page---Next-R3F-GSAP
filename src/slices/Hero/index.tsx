4774958784a09970bc9e88dc0347a18a4e2068d6
ricotech.dev <151479600+rico-webdev@users.noreply.github.com>
2025-08-21 20:42:19 +0200

    style(theme): update global font sizes and colors

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -38,17 +38,17 @@ const Hero: FC<HeroProps> = ({ slice }) => {
       <div className="grid">
         <div className="grid h-screen place-items-center">
           <div className="grid auto-rows-min place-items-center text-center">
-            <h1 className="hero-heading text-sunset overflow-hidden text-7xl leading-[.8] font-black uppercase md:text-[9rem] lg:text-[13rem]">
+            <h1 className="hero-heading text-sunset overflow-hidden text-[20vw] leading-[.8] font-black uppercase md:text-[9rem] lg:text-[13rem]">
               <>
                 {words[0]} <br />
                 {words[1]}
               </>
             </h1>
 
-            <div className="hero-subheading mt-12 text-5xl font-semibold text-sky-950 lg:text-6xl">
+            <div className="hero-subheading mt-15 text-lg font-semibold text-sky-950 lg:text-xl">
               <PrismicRichText field={slice.primary.subheading} />
             </div>
-            <div className="hero-body text-2xl font-normal text-sky-950">
+            <div className="hero-body text-base font-normal text-sky-950 lg:text-lg">
               <PrismicRichText field={slice.primary.body} />
             </div>
             <Button
@@ -64,10 +64,10 @@ const Hero: FC<HeroProps> = ({ slice }) => {
             className="w-full md:hidden"
             field={slice.primary.cans}
           />
-          <h2 className="text-side-heading lg:text8xl text-6xl font-black text-balance text-sky-950 uppercase">
+          <h2 className="text-side-heading lg:text8xl text-2xl font-black text-balance text-sky-950 uppercase">
             {asText(slice.primary.second_heading)}
           </h2>
-          <div className="text-side-body mt-4 max-w-xl text-xl font-normal text-sky-950">
+          <div className="text-side-body mt-4 max-w-xl text-lg font-normal text-sky-950">
             <PrismicRichText field={slice.primary.second_body} />
           </div>
         </div>
